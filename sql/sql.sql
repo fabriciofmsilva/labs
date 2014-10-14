326461748602fec77bc746086070a53f7f0ddf35
fabriciofmsilva <fabriciofmsilva@gmail.com>
2014-10-13 23:28:42 -0300

    Create a SQL

---
+++ sql/sql.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE movies (title VARCHAR(200), year INTEGER);
+
+CREATE TABLE actors (name VARCHAR(50));
+
+INSERT INTO movies VALUES ("The Hangover", 2009);
+
+INSERT INTO actors VALUES ("Zach Galifianakis");
+
+SELECT * FROM movies;
+
+SELECT * FROM actors;
