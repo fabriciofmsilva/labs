a7761193a94e21110f52301eda51fd9ed9973054
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-04-08 00:10:10 -0300

    Update Learn Java with class 6, Universidade XTI

---
+++ java/learn/xti/Variavel.java
@@ -0,0 +1,13 @@
+/**
+ * Apresentar os conceitos de Variável
+ * @author Fabrício Silva
+ */
+public class Variavel {
+  public static void main (String[] args) {
+    String nome = "Fabrício";
+    int idade = 29;
+    boolean casado = false;
+
+    System.out.println(nome);
+  }
+}
