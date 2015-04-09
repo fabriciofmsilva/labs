f02124d1fc92b3fb6e176b7a3fa2a677589b4f5b
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-04-09 20:40:58 -0300

    Add learn Java class Constantes and VariavelReferencia

---
+++ java/learn/xti/VariavelReferencia.java
@@ -0,0 +1,17 @@
+/**
+ * Apresentar os conceitos de Variáveis de Referência
+ * @author Fabrício Silva
+ */
+
+public class VariavelReferencia {
+  static public void main (String[] args) {
+    // Variável Primitiva
+    int x = 7;
+    x = 9;
+
+    // Variável de Referência
+    String y = "XTI";
+    y = "xti.com.br";
+    y = null;
+  }
+}
