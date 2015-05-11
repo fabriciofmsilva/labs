dd2a3a6103fd52543d26b3727fdaa61ad21a97ee
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-05-10 23:14:22 -0300

    Insert new file, Alura, Modelagem de Banco de dados Relacional e SQL, Aula 2

--- alura/sql-1/controle_compras.sql
+++ alura/sql-1/controle_compras.sql
@@ -12,3 +12,9 @@ SELECT * FROM compras WHERE recebido = 0;
 INSERT INTO compras (valor, data, observacoes, recebido) VALUES (100.0, '2010-09-08', 'COMIDA', TRUE);
 SELECT * FROM compras WHERE valor > 5000 OR recebido = 1;
 SELECT * FROM compras WHERE (valor >= 1000 AND valor <= 3000) OR (valor > 5000);
+# mysql -uroot -p controle_compras < cap3.sql
+UPDATE compras SET observacoes = 'PREPARANDO O NATAL' WHERE data = '2010-12-20';
+UPDATE compras SET valor = valor + 10 WHERE data < '2009-06-01';
+UPDATE compras SET observacoes = 'ENTREGUE ANTES DE 2011', recebido = TRUE WHERE data BETWEEN '2009-07-01' AND '2010-07-01';
+DELETE FROM compras WHERE data BETWEEN '2009-03-05' AND '2009-03-20';
+SELECT * FROM compras WHERE NOT valor = 108.00;
