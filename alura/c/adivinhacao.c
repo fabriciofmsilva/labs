c76a96585502babe5fd9dd5ebf61df133675c84d
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-05-14 21:28:39 -0300

    Update files, Alura, Introdução a C, Aula 1

---
+++ alura/c/adivinhacao.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  // imprime o cabecalho do nosso jogo
+  printf("******************************************\n");
+  printf("* Bem vindo ao nosso jogo de adivinhação *\n");
+  printf("******************************************\n");
+}
