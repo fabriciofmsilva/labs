e80f1b331482c558a01f78b87704bc7c23b40639
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-05-16 00:03:57 -0300

    Update file, Alura, Introdução a C, Aula 1

--- alura/c/adivinhacao.c
+++ alura/c/adivinhacao.c
@@ -5,4 +5,8 @@ int main() {
   printf("******************************************\n");
   printf("* Bem vindo ao nosso jogo de adivinhação *\n");
   printf("******************************************\n");
+
+  int numerosecreto = 42;
+
+  printf("O número %d é o secreto. Não conta pra ninguém!\n", numerosecreto);
 }
