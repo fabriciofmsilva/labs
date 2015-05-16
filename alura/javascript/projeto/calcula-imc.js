6283af963b7aaad5ea6a68cabbce8fd43ab7bad1
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-05-15 22:05:51 -0300

    Insert new files, Alura, JavaScript, Aula 3

--- alura/javascript/projeto/calcula-imc.js
+++ alura/javascript/projeto/calcula-imc.js
@@ -1,11 +1,21 @@
 // peso / altura * altura
 
-var peso = 100;
-var altura = 2.0;
-var nome = 'Fulano da Silva';
+var peso = document.getElementById('peso-2').textContent;
+var altura = document.getElementById('altura-2').textContent;
+var nome = document.getElementById('nome-2').textContent;
 
-if (altura != 0) {
-  var imc = peso / (altura * altura); // 25
+var paciente = {
+  peso   : peso,
+  altura : altura,
+  nome   : nome
+};
+
+if (paciente.altura != 0) {
+  var imc = paciente.peso / (paciente.altura * paciente.altura); // 25
+
+  var tdImc = document.getElementById('imc-2');
+  tdImc.textContent = imc;
+
   console.log(imc);
 } else {
   console("Não posso executar uma divisão por 0!");
