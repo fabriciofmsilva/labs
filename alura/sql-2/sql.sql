445805897cc97a2358105dafa7a170b12934ffc6
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-05-17 22:00:13 -0300

    Update files, Alura, Banco de Dados e SQL Complexas, Aula 2

--- alura/sql-2/sql.sql
+++ alura/sql-2/sql.sql
@@ -5,3 +5,8 @@ CREATE DATABASE sql2;
 SELECT * FROM aluno;
 SELECT a.nome FROM aluno a WHERE NOT EXISTS (SELECT m.id FROM matricula m WHERE m.aluno_id = a.id);
 SELECT a.nome FROM aluno a WHERE NOT EXISTS (SELECT m.id FROM matricula m WHERE m.aluno_id = a.id AND m.data > now() - INTERVAL 45 day);
+# aula 2
+SELECT * FROM aluno LIMIT 0,2;
+SELECT * FROM aluno WHERE email LIKE '%.com' LIMIT 0,3;
+SELECT * FROM aluno WHERE email LIKE '%.com' ORDER BY nome LIMIT 0,2;
+SELECT * FROM aluno WHERE nome LIKE '%Silva%';
