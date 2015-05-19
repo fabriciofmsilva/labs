bbca07cdd570fd8570fc63ac6ff80f943ff3630b
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-05-18 21:43:59 -0300

    Update file, Alura, Banco de Dados e SQL Complexas, Aula 3

--- alura/sql-2/sql.sql
+++ alura/sql-2/sql.sql
@@ -10,3 +10,10 @@ SELECT * FROM aluno LIMIT 0,2;
 SELECT * FROM aluno WHERE email LIKE '%.com' LIMIT 0,3;
 SELECT * FROM aluno WHERE email LIKE '%.com' ORDER BY nome LIMIT 0,2;
 SELECT * FROM aluno WHERE nome LIKE '%Silva%';
+# aula 3
+SELECT c.nome, AVG(n.nota) FROM nota n JOIN resposta r ON r.id = n.resposta_id JOIN exercicio e ON e.id = r.exercicio_id JOIN secao s ON s.id = e.secao_id JOIN curso c ON c.id = s.curso_id GROUP BY c.nome;
+SELECT c.nome, COUNT(m.id) FROM curso c JOIN matricula m ON c.id = m.curso_id GROUP BY c.nome;
+SELECT c.nome, AVG(n.nota) FROM nota n JOIN resposta r ON r.id = n.resposta_id JOIN exercicio e ON e.id = r.exercicio_id JOIN secao s ON s.id = e.secao_id JOIN curso c ON c.id = s.curso_id JOIN aluno a ON a.id = r.aluno_id WHERE a.nome LIKE "%Silva" OR a.nome LIKE "%Santos" GROUP BY c.nome;
+SELECT e.pergunta, COUNT(r.id) FROM exercicio e JOIN resposta r ON e.id = r.exercicio_id GROUP BY e.id;
+SELECT e.pergunta, COUNT(r.id) FROM exercicio e JOIN resposta r ON e.id = r.exercicio_id GROUP BY e.id ORDER BY COUNT(r.id) DESC;
+SELECT a.nome, c.nome, AVG(n.nota) FROM nota n JOIN resposta r ON r.id = n.resposta_id JOIN exercicio e ON e.id = r.exercicio_id JOIN secao s ON s.id = e.secao_id JOIN curso c ON c.id = s.curso_id JOIN aluno a ON a.id = r.aluno_id GROUP BY c.nome, a.nome;
