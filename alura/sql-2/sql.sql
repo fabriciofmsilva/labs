1c2589f21cc8693b82ea123cb6a0711f1e9725da
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-05-19 22:50:10 -0300

    pdate file, Alura, Banco de Dados e SQL Complexas, Aula 5

--- alura/sql-2/sql.sql
+++ alura/sql-2/sql.sql
@@ -21,3 +21,7 @@ SELECT a.nome, c.nome, AVG(n.nota) FROM nota n JOIN resposta r ON r.id = n.respo
 SELECT a.nome, c.nome, AVG(n.nota) FROM nota n JOIN resposta r ON r.id = n.resposta_id JOIN exercicio e ON e.id = r.exercicio_id JOIN secao s ON s.id = e.secao_id JOIN curso c ON c.id = s.curso_id JOIN aluno a ON a.id = r.aluno_id GROUP BY c.nome, a.nome HAVING AVG(n.nota) < 5;
 SELECT c.nome, COUNT(m.id) FROM curso c JOIN matricula m ON c.id = m.curso_id GROUP BY c.nome HAVING COUNT(m.id) > 1;
 SELECT c.nome, COUNT(s.id) FROM curso c JOIN secao s ON c.id = s.curso_id GROUP BY c.nome HAVING COUNT(s.id) > 3;
+# aula 5
+SELECT DISTINCT tipo FROM matricula;
+SELECT c.nome, COUNT(m.id) FROM curso c JOIN matricula m ON c.id = m.curso_id WHERE m.tipo IN ('PAGA_PF', 'PAGA_PJ') GROUP BY c.nome;
+SELECT e.pergunta, COUNT(r.id) FROM exercicio e JOIN resposta r ON e.id = r.exercicio_id JOIN secao s on s.id = e.secao_id JOIN curso c on s.curso_id = c.id WHERE c.id IN (1, 3) GROUP BY e.pergunta;
