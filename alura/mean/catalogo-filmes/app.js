1d3103f46dec80eaf1684b2111fcbd644cbc861c
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-05-27 23:15:46 -0300

    Insert new files, Alura, MEAN Stack, Aula 2

--- alura/mean/catalogo-filmes/app.js
+++ alura/mean/catalogo-filmes/app.js
@@ -1,6 +1,8 @@
 // app.js
 var express = require('express');
+var http = require('http');
 var path = require('path');
+var bodyParser = require('body-parser');
 
 // importando arquivo routes/index.js
 var routes = require('./routes');
@@ -8,7 +10,17 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', routes.index);
+app.get('/lista', routes.lista);
+app.post('/grava', routes.grava);
+
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
