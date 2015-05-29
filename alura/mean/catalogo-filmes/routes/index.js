4201da3f53664f82177442de68729e117118f080
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-05-29 00:10:39 -0300

    Insert new files, Alura, MEAN Stack, Aula 3

--- alura/mean/catalogo-filmes/routes/index.js
+++ alura/mean/catalogo-filmes/routes/index.js
@@ -1,18 +1,22 @@
+var Filme = require('../models/filme.js');
+
 exports.index = function(req, res) {
   res.render('index');
 };
 
 exports.lista = function(req, res) {
-  res.json({
-    titulo: 'Gangues de Nova Iorque',
-    diretor: 'Martin Scorsese',
-    ano: 2002
+  Filme.find({}, function(erro, filmes) {
+    res.json({filmes: filmes});
   });
 };
 
 exports.grava = function(req, res) {
-  var filme = req.body;
-  console.log(filme);
+  var filme = new Filme(req.body);
 
-  res.send('Filme ' + filme.titulo + ' recebido no servidor');
+  filme.save(function(error, filme) {
+    if(error) {
+      return console.log(error);
+    }
+    res.send('Filme ' + filme.titulo + ' adicionado com sucesso!');
+  });
 };
