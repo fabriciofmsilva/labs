baa06da38d8a0aee3f0d45eba987681b91b2942c
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-06-15 22:41:46 -0300

    Insert new files, Alura, Avançando no HTML e CSS, Aula 13

--- alura/c/adivinhacao.c
+++ alura/c/adivinhacao.c
@@ -13,6 +13,15 @@ int main() {
 
   printf("Qual é o seu chute? ");
   scanf("%d", &chute);
-  printf("Seu chute foi %d", chute);
+  printf("Seu chute foi %d\n", chute);
+
+  if(chute == numerosecreto) {
+    printf("Parabéns! Você acertou!\n");
+    printf("Jogue de novo, você é um bom jogador!\n");
+  }
+  else {
+    printf("Você errou!\n");
+    printf("Mas não desanime, tente de novo!\n");
+  }
 
 }
