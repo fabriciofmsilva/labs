b7591601153e60d09d4e695072b1add925800771
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-06-17 22:22:28 -0300

    Insert new files, Alura, PHP, MySQL e Fundamentos da Web, Aula 1

---
+++ alura/php-1/loja/cabecalho.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Minha Loja</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/loja.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="principal">
---
+++ alura/php-1/loja/produto-formulario.php
@@ -0,0 +1,11 @@
+<?php include("cabecalho.php") ?>
+<h1>Formulário de cadastro</h1>
+<form action="adiciona-produto.php">
+  Nome:
+  <input name="nome" type="text"><br>
+  Preco:
+  <input name="preco" type="number"><br>
+
+  <input type="submit" value="Cadastrar">
+</form>
+<?php include("rodape.php") ?>
