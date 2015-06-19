e70a9fecb1334081c139740eecd40826952fd0fe
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-06-18 22:23:46 -0300

    Update files, Alura, PHP, MySQL e Fundamentos da Web, Aula 2

---
+++ alura/php-1/banco.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE loja;
+CREATE TABLE produtos (id INTEGER AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255), preco DECIMAL(10, 2));
+SELECT * FROM produtos;
+INSERT INTO produtos VALUES (1, 'carro', 20000);
+INSERT INTO produtos VALUES (2, 'morocicleta', 10000);
+INSERT INTO produtos (nome, preco) VALUES ('bicicleta', 300);
--- alura/php-1/loja/adiciona-produto.php
+++ alura/php-1/loja/adiciona-produto.php
@@ -2,6 +2,15 @@
 <?php
   $nome = $_GET["nome"];
   $preco = $_GET["preco"];
-?>
+
+  $conexao = mysqli_connect('localhost', 'root', '', 'loja');
+
+  $query = "INSERT INTO produtos (nome, preco) VALUES ('{$nome}', {$preco})";
+
+if(mysqli_query($conexao, $query)) { ?>
   <p class="alert-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+<?php } else { ?>
+  <p class="alert-danger">O produto <?= $nome ?> não foi adicionado.</p>
+<?php } ?>
+
 <?php include("rodape.php") ?>
