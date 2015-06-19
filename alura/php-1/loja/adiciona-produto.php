1bd1f774a2ad19c36882d8bc03bca17b26409145
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-06-19 00:20:14 -0300

    Update files, Alura, PHP, MySQL e Fundamentos da Web, Aula 3

--- alura/php-1/loja/adiciona-produto.php
+++ alura/php-1/loja/adiciona-produto.php
@@ -1,16 +1,21 @@
 <?php include("cabecalho.php") ?>
 <?php
-  $nome = $_GET["nome"];
-  $preco = $_GET["preco"];
-
-  $conexao = mysqli_connect('localhost', 'root', '', 'loja');
-
+function insereProduto($conexao, $nome, $preco) {
   $query = "INSERT INTO produtos (nome, preco) VALUES ('{$nome}', {$preco})";
+  $resultadoDaInsercao = mysqli_query($conexao, $query);
+  return $resultadoDaInsercao;
+}
 
-if(mysqli_query($conexao, $query)) { ?>
-  <p class="alert-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
-<?php } else { ?>
-  <p class="alert-danger">O produto <?= $nome ?> não foi adicionado.</p>
+$nome = $_GET["nome"];
+$preco = $_GET["preco"];
+$conexao = mysqli_connect('localhost', 'root', '', 'loja');
+
+if(insereProduto($conexao, $nome, $preco)) { ?>
+  <p class="text-success">O produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+<?php } else {
+  $msg = mysqli_error($conexao);
+?>
+  <p class="text-danger">O produto <?= $nome ?> não foi adicionado: <?= $msg ?></p>
 <?php } ?>
 
 <?php include("rodape.php") ?>
--- alura/php-1/loja/produto-formulario.php
+++ alura/php-1/loja/produto-formulario.php
@@ -1,11 +1,18 @@
 <?php include("cabecalho.php") ?>
 <h1>Formulário de cadastro</h1>
 <form action="adiciona-produto.php">
-  Nome:
-  <input name="nome" type="text"><br>
-  Preco:
-  <input name="preco" type="number"><br>
-
-  <input type="submit" value="Cadastrar">
+  <table class="table">
+    <tr>
+      <td>Nome:</td>
+      <td><input class="form-control" name="nome" type="text"></td>
+    </tr>
+    <tr>
+      <td>Preco:</td>
+      <td><input class="form-control" name="preco" type="number"></td>
+    </tr>
+    <tr>
+      <td><button class="btn btn-primary" type="submit">Cadastrar</button></td>
+    </tr>
+  </table>
 </form>
 <?php include("rodape.php") ?>
