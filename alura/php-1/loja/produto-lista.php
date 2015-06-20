354d3f33105731d957978df49fb2ad7bf56831a7
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-06-20 14:29:50 -0300

    Update files, Alura, PHP, MySQL e Fundamentos da Web, Aula 10

--- alura/php-1/loja/produto-lista.php
+++ alura/php-1/loja/produto-lista.php
@@ -22,6 +22,7 @@
     <td><?= $produto['preco'] ?></td>
     <td><?= substr($produto['descricao'], 0, 40) ?></td>
     <td><?= $produto['categoria_nome'] ?></td>
+    <td><a class="btn btn-primary" href="produto-altera-formulario.php?id=<?=$produto['id']?>">alterar</a></td>
     <td>
       <form action="remove-produto.php" method="post">
         <input name="id" type="hidden" value="<?=$produto['id']?>">
