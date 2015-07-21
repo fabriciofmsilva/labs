48ba0fa8d8aad77e8f03230b7d504b1feb4c940c
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-07-20 22:56:30 -0300

    Update files, Alura, Introdução a C - Parte 2, Aula 2.1

---
+++ alura/c/forca.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int notas[10];
+
+  notas[0] = 10;
+  notas[2] = 9;
+  notas[3] = 8;
+  notas[9] = 4;
+
+  printf("%d %d %d\n", notas[0], notas[2], notas[9]);
+}
