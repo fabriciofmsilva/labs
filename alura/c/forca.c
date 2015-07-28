e9b5d3e4c0d690b50cc9a04b2aaec37381e22854
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-07-27 22:49:04 -0300

    Update files, Alura, Introdução a C - Parte 2, Aula 2.3

--- alura/c/forca.c
+++ alura/c/forca.c
@@ -5,19 +5,12 @@ int main() {
 
   sprintf(palavrasecreta, "MELANCIA");
 
-  printf("%s\n", palavrasecreta);
+  int acertou = 0;
+  int enforcou = 0;
 
-  /*
-  palavrasecreta[0] = 'M';
-  palavrasecreta[1] = 'E';
-  palavrasecreta[2] = 'L';
-  palavrasecreta[3] = 'A';
-  palavrasecreta[4] = 'N';
-  palavrasecreta[5] = 'C';
-  palavrasecreta[6] = 'I';
-  palavrasecreta[7] = 'A';
-  palavrasecreta[8] = '\0';
+  do {
+    // comecar o nosso jogo!!
 
-  printf("%c%c%c%c%c%c%c%c\n", palavrasecreta[0], palavrasecreta[1], palavrasecreta[2], palavrasecreta[3], palavrasecreta[4], palavrasecreta[5], palavrasecreta[6], palavrasecreta[7]);
-  */
+  } while(!acertou && !enforcou);
+
 }
