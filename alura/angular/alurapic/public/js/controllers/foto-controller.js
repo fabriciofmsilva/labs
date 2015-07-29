61e92a90143bbb187716296395eb68c76cc912e6
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-07-29 00:01:06 -0300

    Update files, Alura, Angular, Aula 9

--- alura/angular/alurapic/public/js/controllers/foto-controller.js
+++ alura/angular/alurapic/public/js/controllers/foto-controller.js
@@ -1,41 +1,37 @@
 angular.module('Alurapic')
-.controller('FotoController', function ($scope, $http, $routeParams){
+.controller('FotoController', function ($scope, recursoFoto, $routeParams) {
 
   $scope.foto = {};
   $scope.mensagem = '';
 
   if($routeParams.fotoId) {
-    $http.get('v1/fotos/' + $routeParams.fotoId)
-      .success(function(foto) {
+    recursoFoto.get({fotoId : $routeParams.fotoId}, function(foto) {
         $scope.foto = foto;
-      })
-      .error(function(erro) {
+    }, function() {
         console.log(erro);
         $scope.mensagem = 'Não foi possível obter a foto';
-      });
+    });
   }
 
   $scope.submeter = function() {
     if($scope.formulario.$valid) {
       if($scope.foto._id) {
-        $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
-          .success(function() {
-            $scope.mensagem = 'A foto ' + $scope.foto.titulo + ' foi alterada com sucesso!';
-          })
-          .error(function(erro) {
-            console.log(erro);
-            $scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo;
-          });
+
+        recursoFoto.update({fotoId : $scope.foto._id}, $scope.foto, function() {
+          $scope.mensagem = 'A foto ' + $scope.foto.titulo + ' foi alterada com sucesso!';
+        }, function(erro) {
+          $scope.mensagem = 'Não foi possível alterar a foto ' + $scope.foto.titulo;
+        });
+
       } else {
-        $http.post('v1/fotos', $scope.foto)
-          .success(function() {
+
+        recursoFoto.save($scope.foto, function() {
             $scope.foto = {};
             $scope.mensagem = 'Foto incluida com sucesso';
-          })
-          .error(function(erro) {
+        }, function(erro) {
             $scope.mensagem = 'Não foi possível incluir a foto';
             console.log(erro);
-          });
+        });
       }
     }
   };
--- alura/angular/alurapic/public/js/main.js
+++ alura/angular/alurapic/public/js/main.js
@@ -1,4 +1,4 @@
-angular.module('Alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+angular.module('Alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute', 'meusServicos'])
 .config(function($routeProvider, $locationProvider) {
 
   $locationProvider.html5Mode(true);
