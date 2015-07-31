fe92271ae3fbf3a3d8baad7c04386456538bf85a
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-07-30 21:21:54 -0300

    Update files, Alura, Design Patterns em PHP para Bons Programadores 2, aula 3

---
+++ alura/php-design-patterns2/Contrato.php
@@ -0,0 +1,29 @@
+<?php
+  class Contrato {
+    private $nome;
+    private $data;
+    private $tipo;
+
+    function __construct($novoNome, $data, $tipo = null) {
+      $this->nome = $novoNome;
+      $this->data = $data;
+      if(!is_null($tipo)) {
+        $this->tipo = $tipo;
+      } else {
+        $this->tipo = new Novo();
+      }
+    }
+
+    public function setTipo(TipoDeContrato $novoTipo) {
+      $this->tipo = $novoTipo;
+    }
+
+    public function avanca() {
+      $this->tipo->avanca($this);
+    }
+
+    public function salvaEstado() {
+      return new Estado(new Contrato($this->nome, $this->data, $this->tipo));
+    }
+  }
+?>
