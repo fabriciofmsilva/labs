f5209baeb2a37d1654e642e442c8c961bf662500
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-08-02 16:38:04 -0300

    Update files, Alura, Introdução a C - Parte 2, Aula 2.5

--- alura/c/forca.c
+++ alura/c/forca.c
@@ -9,15 +9,36 @@ int main() {
   int acertou = 0;
   int enforcou = 0;
 
+  char chutes[26];
+  int tentativas = 0;
+
   do {
-    char chute;
-    scanf("%c", &chute);
 
     for(int i = 0; i < strlen(palavrasecreta); i++) {
-      if(palavrasecreta[i] == chute) {
-        printf("A posição %d tem essa letra!\n", i);
+
+      int achou = 0;
+
+      for(int j = 0; j < tentativas; j++) {
+        if(chutes[j] == palavrasecreta[i]) {
+          achou = 1;
+          break;
+        }
+      }
+
+      if(achou) {
+        printf("%c ", palavrasecreta[i]);
+      } else {
+        printf("_ ");
       }
     }
+
+    printf("\n");
+
+    char chute;
+    scanf(" %c", &chute);
+
+    chutes[tentativas] = chute;
+    tentativas++;
 
   } while(!acertou && !enforcou);
 
