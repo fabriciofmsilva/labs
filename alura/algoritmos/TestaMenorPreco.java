8af6b58630488251575c46f3f1be8869506120a7
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-08-11 22:50:38 -0300

    Insert new files, Alura, Projeto de Algoritmos 1, Aula 9

--- alura/algoritmos/TestaMenorPreco.java
+++ alura/algoritmos/TestaMenorPreco.java
@@ -2,23 +2,25 @@ public class TestaMenorPreco {
 
   public static void main(String[] args) {
 
-    double precos[] = new double[5];
-    precos[0] = 1000000;
-    precos[1] = 46000;
-    precos[2] = 16000;
-    precos[3] = 46000;
-    precos[4] = 17000;
+    Produto produtos[] = new Produto[5];
+    produtos[0] = new Produto("Lamborghini", 1000000);
+    produtos[1] = new Produto("Jipe", 46000);
+    produtos[2] = new Produto("Brasília", 16000);
+    produtos[3] = new Produto("Smart", 46000);
+    produtos[4] = new Produto("Fusca", 17000);
 
     int maisBarato = 0;
 
-    for(int atual = 0; atual < precos.length; atual++) {
-      if(precos[atual] < precos[maisBarato]) {
+    for(int atual = 0; atual <= 4; atual++) {
+      if(produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
         maisBarato = atual;
       }
     }
 
     System.out.println(maisBarato);
-    System.out.println("O carro mais barato custa " + precos[maisBarato]);
+    System.out.println("O carro " + produtos[maisBarato].getNome()
+                        + " é o mais barato, e custa "
+                        + produtos[maisBarato].getPreco());
 
   }
 
