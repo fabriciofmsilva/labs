1843648020067d304d85f2f9eac840a0da1df24e
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-08-13 23:22:10 -0300

    Insert new files, AngularJS para Zumbis, Aula 16

--- courses/angular/ng-cep-validator.directive.js
+++ courses/angular/ng-cep-validator.directive.js
@@ -1,4 +1,4 @@
-angular.module('app').directive('ngCepValidator', function($http, $rootScope) {
+angular.module('app').directive('ngCepValidator', function(CepService, $rootScope) {
   return {
     restrict: 'A',
     require: 'ngModel',
@@ -6,10 +6,9 @@ angular.module('app').directive('ngCepValidator', function($http, $rootScope) {
       $scope.$watch($attrs.ngModel, function(value) {
         if(value) {
           if(value.match(/^[0-9]{5}-[0-9]{3}$/)) {
-            $http.get('http://api.postmon.com.br/v1/cep/' + value)
-              .then(function(response) {
-                $rootScope.$broadcast('cep', response.data);
-              });
+            CepService.get(value).then(function(response) {
+              $rootScope.$broadcast('cep', response.data);
+            });
             ngModel.$setValidity($attrs.ngModel, true);
           } else {
             ngModel.$setValidity($attrs.ngModel, false);
