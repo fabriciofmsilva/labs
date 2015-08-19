ed2e113a2d0c1734f6221c3f4c50a4467bf2305e
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-08-18 23:32:17 -0300

    Insert new files, Alura, JDBC e banco de dados em Java, Aula 1

---
+++ alura/java-bd/loja-virtual/TesteListagem.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaListagem {
+
+  public static void main(String[] args) throws SQLException {
+    Connection connection = DriverManager.getConnection("jdbc:hsqldb://localhost/loja-virtual", "SA", "");
+
+    Statement statement = connection.createStatemet();
+    boolean resultado = statement.execute("select * from Produto");
+    ResultSet resultSet = statement.getResultSet();
+    while(resultSet.next()) {
+      int id = resultSet.getInt("id");
+      String nome = resultSet.getString("nome");
+      String descricao = resultSet.getString("descricao");
+
+      System.out.println(id);
+      System.out.println(nome);
+      System.out.println(descricao);
+    }
+    resultSet.close();
+    statement.close();
+
+    System.out.println(resultado);
+
+    connection.close();
+  }
+}
