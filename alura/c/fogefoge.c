dd41d66b3c1bfd6f51036df06376d30924e961c6
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-08-20 22:07:41 -0300

    Alura, Introdução a C - Parte 3, Aula 1.3

--- alura/c/fogefoge.c
+++ alura/c/fogefoge.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
   // matriz de 5 x 10
-  char mapa[5][10];
+  char mapa[5][10+1];
 
-  mapa[0][0] = '|';
-  mapa[4][9] = '@';
+  FILE* f;
+  f = fopen("mapa.txt", "r");
+  if(f == 0) {
+    printf("Erro na leitura do mapa\n");
+    exit(1);
+  }
 
-  printf("%c %c\n", mapa[0][0], mapa[4][9]);
+  for(int i = 0; i < 5; i++) {
+    fscanf(f, "%s", mapa[i]);
+  }
+
+  // for(int i = 0; i < 5; i++) {
+  //   for(int j = 0; j < 10; j++) {
+  //     printf("%c", mapa[i][j]);
+  //   }
+  //   printf("\n");
+  // }
+
+  for(int i = 0; i < 5; i++) {
+    printf("%s\n", mapa[i]);
+  }
+
+  fclose(f);
 }
