d3bd245fd9dd935a74a685fa823cb1f24bb9bc12
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-08-20 21:57:22 -0300

    Insert new files, Alura, JDBC e banco de dados em Java, Aula 3

--- alura/java-bd/loja-virtual/TestaInsercao.java
+++ alura/java-bd/loja-virtual/TestaInsercao.java
@@ -5,9 +5,18 @@ import java.sql.Statement;
 public class TestaInsercao {
 
   public static void main(String[] args) throws SQLException {
+    String nome = "Notebook";
+    String descricao = "  Notebook i5";
+
     Connection connection = Database.getConnection();
-    Statement statement = connection.createStatement();
-    boolean resultado = statement.execute("insert into Produto (nome, descricao) values('Notebook', 'Notebook i5'", Statement.RETURN_GENERATE_KEYS);
+
+    String sql = "insert into Produto (nome, descricao) values(?, ?)";
+    PreparedStatement statement = connection.prepareStatement(sql,
+        Statement.RETURN_GENERATE_KEYS);
+    statement.setString(1, nome);
+    statement.setString(2, descricao);
+
+    boolean resultado = statement.execute();
     System.out.println(resultado);
 
     ResultSet resultSet = statement.getGeneratedKeys();
@@ -16,6 +25,7 @@ public class TestaInsercao {
       System.out.println(id + " gerado");
     }
 
+    resultSet.close();
     statement.close();
     connection.close();
   }
