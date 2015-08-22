1258d62eb32128afbad2fdf46d5f0a5f91d31daa
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-08-22 19:16:27 -0300

    Alura, Introdução a C - Parte 3, Aula 2.2

--- alura/c/fogefoge.h
+++ alura/c/fogefoge.h
@@ -1,3 +1,13 @@
 void liberamapa();
 void alocamapa();
 void lemapa();
+
+void move(char direcao);
+int acabou();
+void imprimemapa();
+
+struct mapa {
+  char** matriz;
+  int linhas;
+  int colunas;
+};
