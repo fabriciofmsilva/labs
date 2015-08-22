93cdb03d82ceeea186252fbc6975789286ccfaf0
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-08-22 19:08:21 -0300

    Alura, Introdução a C - Parte 3, Aula 2.1

--- alura/c/fogefoge.c
+++ alura/c/fogefoge.c
@@ -38,13 +38,62 @@ void lemapa() {
   fclose(f);
 }
 
+void imprimemapa() {
+  for(int i = 0; i < 5; i++) {
+    printf("%s\n", mapa[i]);
+  }
+}
+
+int acabou() {
+  return 0;
+}
+
+void move(char direcao) {
+  int x;
+  int y;
+
+  // acha a posicao do foge foge
+  for(int i = 0; i < linhas; i++) {
+    for(int j = 0; j < colunas; j++) {
+      if(mapa[i][j] == '@') {
+        x= i;
+        y = j;
+        break;
+      }
+    }
+  }
+
+  switch(direcao) {
+    case 'a':
+      mapa[x][y-1] = '@';
+      break;
+    case 'w':
+      mapa[x-1][y] = '@';
+      break;
+    case 's':
+      mapa[x+1][y] = '@';
+      break;
+    case 'd':
+      mapa[x][y+1] = '@';
+      break;
+  }
+
+  mapa[x][y] = '.';
+
+}
+
 int main() {
 
   lemapa();
 
-  for(int i = 0; i < 5; i++) {
-    printf("%s\n", mapa[i]);
-  }
+  do {
+    imprimemapa();
+
+    char comando;
+    scanf(" %c", &comando);
+    move(comando);
+
+  } while(!acabou());
 
   liberamapa();
 
