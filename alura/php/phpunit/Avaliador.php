0dc1c6eb65e4fcc29ba9153d81aa7f073f696b29
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-09-01 23:09:15 -0300

    Alura, Testes e TDD com PHP Unit, Aula 2.2

--- alura/php/phpunit/Avaliador.php
+++ alura/php/phpunit/Avaliador.php
@@ -1,9 +1,10 @@
 <?php
   class Avaliador {
 
-    public $maiorValor = -9999;
-    public $menorValor = INF;
-    public $media = 0;
+    private $maiorValor = -9999;
+    private $menorValor = INF;
+    private $media = 0;
+    private $maiores;
 
     public function avalia(Leilao $leilao) {
       $total = 0;
@@ -16,7 +17,25 @@
         }
         $total += $lance->getValor();
       }
-      $this->media = $total / count($leilao->getLances());
+
+      if($total != 0) {
+        $this->media = $total / count($leilao->getLances());
+      }
+
+      $this->pegaOsMaioresNo($leilao);
+    }
+
+    public function pegaOsMaioresNo(Leilao $leilao) {
+      $lances = $leilao->getLances();
+
+      usort($lances, function($a, $b) {
+        if($a->getValor() == $b->getValor()) {
+          return 0;
+        }
+        return $a->getValor() < $b->getValor() ? 1 : -1;
+      });
+
+      $this->maiores = array_slice($lances, 0, 3);
     }
 
     public function getMaiorLance() {
@@ -29,6 +48,10 @@
 
     public function getMedia() {
       return $this->media;
+    }
+
+    public function getMaiores() {
+      return $this->maiores;
     }
 
   }
--- alura/php/phpunit/test/AvaliadorTest.php
+++ alura/php/phpunit/test/AvaliadorTest.php
@@ -63,6 +63,29 @@
       $this->assertEquals($menorEsperado, $leiloeiro->getMenorLance());
     }
 
+    public function testDeveAceitarLancesEmOrdemAleatoria() {
+      $leilao = new Leilao("Playstation 4");
+
+      $renan = new Usuario("Renan");
+      $caio = new Usuario("Caio");
+      $felipe = new Usuario("Felipe");
+
+      $leilao->propoe(new Lance($felipe, 200));
+      $leilao->propoe(new Lance($caio, 450));
+      $leilao->propoe(new Lance($renan, 120));
+      $leilao->propoe(new Lance($felipe, 700));
+      $leilao->propoe(new Lance($caio, 630));
+      $leilao->propoe(new Lance($felipe, 230));
+
+      $leiloeiro = new Avaliador();
+      $leiloeiro->avalia($leilao);
+
+      $maiorEsperado = 700;
+      $menorEsperado = 120;
+      $this->assertEquals($maiorEsperado, $leiloeiro->getMaiorLance());
+      $this->assertEquals($menorEsperado, $leiloeiro->getMenorLance());
+    }
+
     public function testDeveCalcularAMedia() {
       $leilao = new Leilao("Playstation 4");
 
@@ -79,6 +102,57 @@
 
       $media = 400;
       $this->assertEquals($media, $leiloeiro->getMedia(), 0.0001);
+    }
+
+    public function testDeveEncontrarOsTresMaioresLances() {
+      $leilao = new Leilao("Playstation 4");
+
+      $renan = new Usuario("Renan");
+      $mauricio = new Usuario("Mauricio");
+
+      $leilao->propoe(new Lance($renan, 200));
+      $leilao->propoe(new Lance($mauricio, 300));
+      $leilao->propoe(new Lance($renan, 400));
+      $leilao->propoe(new Lance($mauricio, 500));
+
+      $leiloeiro = new Avaliador();
+      $leiloeiro->avalia($leilao);
+
+      $this->assertEquals(3, count($leiloeiro->getMaiores()));
+      $this->assertEquals(500, $leiloeiro->getMaiores()[0]->getValor());
+      $this->assertEquals(400, $leiloeiro->getMaiores()[1]->getValor());
+      $this->assertEquals(300, $leiloeiro->getMaiores()[2]->getValor());
+    }
+
+    public function testDeveDevolverTodosLancesCasoNaoHajaNoMinimo3() {
+      $leilao = new Leilao("Playstation 4");
+
+      $renan = new Usuario("Renan");
+      $mauricio = new Usuario("Mauricio");
+
+      $leilao->propoe(new Lance($renan, 200));
+      $leilao->propoe(new Lance($mauricio, 300));
+
+      $leiloeiro = new Avaliador();
+      $leiloeiro->avalia($leilao);
+
+      $this->assertEquals(2, count($leiloeiro->getMaiores()));
+      $this->assertEquals(300, $leiloeiro->getMaiores()[0]->getValor());
+      $this->assertEquals(200, $leiloeiro->getMaiores()[1]->getValor());
+    }
+
+    public function testDeveDevolverListaVaziaCasoNaoHajaLances() {
+      $leilao = new Leilao("Playstation 4");
+
+      $renan = new Usuario("Renan");
+      $mauricio = new Usuario("Mauricio");
+
+      $leiloeiro = new Avaliador();
+      $leiloeiro->avalia($leilao);
+
+      $maiores = $leiloeiro->getMaiores();
+
+      $this->assertEquals(0, count($maiores));
     }
 
   }
