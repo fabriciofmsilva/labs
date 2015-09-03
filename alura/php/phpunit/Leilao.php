ffcbc261f80a09a0b1a950eb7ff33b3905efd919
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-09-03 00:02:20 -0300

    Alura, Testes e TDD com PHP Unit, Aula 3.2

--- alura/php/phpunit/Leilao.php
+++ alura/php/phpunit/Leilao.php
@@ -9,9 +9,42 @@
 		}
 
     public function propoe(Lance $lance) {
-      if(count($this->lances) == 0 ||
-         $this->pegaUltimoLance()->getUsuario() != $lance->getUsuario()) {
+      if(count($this->lances) == 0 || $this->podeDarLance($lance->getUsuario())) {
         $this->lances[] = $lance;
+      }
+    }
+
+    private function podeDarLance(Usuario $usuario) {
+
+      $total = $this->contaLancesDo($usuario);
+
+      return $this->pegaUltimoLance()->getUsuario() != $usuario && $total < 5;
+
+    }
+
+    private function contaLancesDo(Usuario $usuario) {
+      $total = 0;
+      foreach($this->lances as $lanceAtual) {
+        if($lanceAtual->getUsuario() == $usuario) {
+          $total++;
+        }
+      }
+      return $total;
+    }
+
+    private function ultimoLanceDo(Usuario $usuario) {
+      $ultimo = null;
+      foreach($this->lances as $lance) {
+        if($lance->getUsuario()->getNome() == $usuario->getNome() ) $ultimo = $lance;
+      }
+
+      return $ultimo;
+    }
+
+    public function dobraLance(Usuario $usuario) {
+      $ultimoLance = $this->ultimoLanceDo($usuario);
+      if($ultimoLance!=null) {
+        $this->propoe(new Lance($usuario, $ultimoLance->getValor()*2));
       }
     }
 
--- alura/php/phpunit/test/LeilaoTest.php
+++ alura/php/phpunit/test/LeilaoTest.php
@@ -35,6 +35,51 @@
 
     }
 
+    public function testDeveDarNoMaximoCincoLances() {
+
+      $leilao = new Leilao("Macbook caro");
+
+      $jobs = new Usuario("Jobs");
+      $gates = new Usuario("Gates");
+
+      $leilao->propoe(new Lance($jobs, 2000));
+      $leilao->propoe(new Lance($gates, 3000));
+      $leilao->propoe(new Lance($jobs, 4000));
+      $leilao->propoe(new Lance($gates, 5000));
+      $leilao->propoe(new Lance($jobs, 6000));
+      $leilao->propoe(new Lance($gates, 7000));
+      $leilao->propoe(new Lance($jobs, 8000));
+      $leilao->propoe(new Lance($gates, 9000));
+      $leilao->propoe(new Lance($jobs, 10000));
+      $leilao->propoe(new Lance($gates, 11000));
+      // deve ser ignorado
+      $leilao->propoe(new Lance($jobs, 12000));
+
+      $this->assertEquals(10, count($leilao->getLances()));
+      $this->assertEquals(11000, $leilao->getLances()[9]->getValor());
+    }
+
+    public function testDeveDobrarOUltimoLanceDado() {
+      $leilao = new Leilao("Macbook Pro 15");
+      $steveJobs = new Usuario("Steve Jobs");
+      $billGates = new Usuario("Bill Gates");
+
+      $leilao->propoe(new Lance($steveJobs, 2000));
+      $leilao->propoe(new Lance($billGates, 3000));
+      $leilao->dobraLance($steveJobs);
+
+      $this->assertEquals(4000, $leilao->getLances()[2]->getValor(), 0.00001);
+    }
+
+    public function testNaoDeveDobrarCasoNaoHajaLanceAnterior() {
+      $leilao = new Leilao("Macbook Pro 15");
+      $steveJobs = new Usuario("Steve Jobs");
+
+      $leilao->dobraLance($steveJobs);
+
+      $this->assertEquals(0, count($leilao->getLances()));
+    }
+
   }
 
 ?>
