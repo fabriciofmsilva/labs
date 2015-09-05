c2ae52d2dfa48beaeb0fb9972316a6892fb1f8b4
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-09-05 12:24:31 -0300

    Alura, C# e Tópicos Avançados, Aula 5

--- alura/c-sharp/Form1.cs
+++ alura/c-sharp/Form1.cs
@@ -16,16 +16,36 @@ namespace OiMundo
       InitializeComponent();
     }
 
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
     private void button1_Click(Object sender, EventArgs e)
     {
-      Cliente cliente1 = new Cliente("Guilherme");
-      cliente1.Rg = "1234-5";
+      var contas = new List<Conta>();
+      var c1 = new ContaCorrente();
+      c1.Titular = "Mauricio";
+      contas.Add(c1);
 
-      Cliente cliente2 = new Cliente("Guilherme");
-      cliente2.Rg = "1234-5";
+      Conta copiaC1 = contas[0];
 
-      cliente1.Equals(cliente2);
-      cliente1.ToString();
+      var c2 = new ContaCorrente();
+      c2.Titular = "Victor";
+
+      MessageBox.Show("Está lá: " + contas.Contains(c1));
+      MessageBox.Show("Está lá: " + contas.Contains(c2));
+
+      contas.Add(c2);
+      //contas.Remove(c1);
+      contas.RemoveAt(0);
+
+      MessageBox.Show("numero de contas: " + contas.Count);
+
+      foreach(var c in contas)
+      {
+        MessageBox.Show(c.Titular);
+      }
     }
   }
 }
