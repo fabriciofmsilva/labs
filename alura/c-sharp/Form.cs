31a63661a74082b898840b30a1cd9bdc253c352f
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-09-08 20:49:12 -0300

    Alura, C# e Tópicos Avançados, Aula 8

---
+++ alura/c-sharp/Form.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threeding.Tasks;
+using System.Windows.Forms;
+
+namespace Editor
+{
+  public partial class Form : Form
+  {
+    public Form()
+    {
+      InitializeComponent();
+    }
+
+    private void Form_Load(object sender, EventArgs e)
+    {
+      if(File.Exists("entrada.txt")) {
+        Stream entrada = File.Open("entrada.txt", FileMode.Open);
+        StreamReader leitor = new StreamReader(entrada);
+        string linha = leitor.ReadLine();
+
+        while(linha != null)
+        {
+          texto.Text += linha;
+          linha = leitor.ReadLin();
+        }
+
+        leitor.Close();
+        entrada.Close();
+      }
+    }
+  }
+}
