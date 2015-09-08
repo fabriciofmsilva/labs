71ffcc2e21a941780ad6a18e8ddbd15ca264c08a
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-09-07 21:25:11 -0300

    Alura, Projeto de Algoritmos 2, Aula 35

---
+++ alura/algoritmos/TestaPivota.java
@@ -0,0 +1,47 @@
+public class TestaPivota {
+
+  public static void main(String[] args) {
+    Nota guilherme = new Nota("guilherme", 7);
+    Nota[] notas = {
+      new Nota("andre", 4),
+      new Nota("carlos", 8.5),
+      new Nota("ana", 10),
+      new Nota("jonas", 3),
+      new Nota("juliana", 6.7),
+      new Nota("lucia", 9.3),
+      new Nota("paulo", 9),
+      new Nota("mariana", 5),
+      guilherme
+    };
+
+    quebraNoPivo(notas, 0, notas.length);
+
+    for(int atual = 0; atual < notas.length; atual++) {
+      Nota nota = notas[atual];
+      System.out.println(nota.getAluno() + " " + nota.getValor());
+    }
+
+  }
+
+  private static void quebraNoPivo(Nota[] notas, int inicial, int termino) {
+    int menoresEncontrados = 0;
+
+    Nota pivo = notas[termino - 1];
+    for(int analisando = 0; analisando < termino - 1; analisando++) {
+      Nota atual = notas[analisando];
+      if(atual.getValor() <= pivo.getValor()) {
+        menoresEncontrados++;
+      }
+    }
+    troca(notas, termino - 1, menoresEncontrados);
+
+  }
+
+  private static void troca(Nota[] notas, int de, int para) {
+    Nota nota1 = notas[de];
+    Nota nota2 = notas[para];
+    notas[para] = nota1;
+    notas[de] = nota2;
+  }
+
+}
