74da9923dff0f1a3b2ac010b73d2d8ebf31e9b0d
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-09-20 15:52:24 -0300

    Alura, Testes de Unidade e TDD, Aula 2

--- alura/teste/teste-de-unidade/src/br/com/caelum/leilao/dominio/Lance.java
+++ alura/teste/teste-de-unidade/src/br/com/caelum/leilao/dominio/Lance.java
@@ -18,6 +18,4 @@ public class Lance {
 		return valor;
 	}
 	
-	
-	
 }
--- alura/teste/teste-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
+++ alura/teste/teste-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
@@ -1,5 +1,10 @@
 package br.com.caelum.leilao.servico;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 
@@ -7,7 +12,8 @@ public class Avaliador {
 	
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
 	private double menorDeTodos = Double.POSITIVE_INFINITY;
-	private double media = 0;   
+	private double media = 0;
+	private List<Lance> maiores;   
 
 	public void avalia(Leilao leilao) {
 		double total = 0;
@@ -21,6 +27,20 @@ public class Avaliador {
 			total += lance.getValor();
 		}
 		media = total / leilao.getLances().size();
+		
+		maiores = new ArrayList<Lance>(leilao.getLances());
+		Collections.sort(maiores, new Comparator<Lance>() {
+			public int compare(Lance o1, Lance o2) {
+				if(o1.getValor() < o2.getValor()) {
+					return 1;
+				}
+				if(o1.getValor() > o2.getValor()) {
+					return -1;
+				}
+				return 0;
+			}
+		});
+		maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
 	}
 	
 	public double getMaiorLance() {
@@ -33,6 +53,10 @@ public class Avaliador {
 	
 	public double getMedia() {
 		return media;
+	}
+	
+	public List<Lance> getTresMaiores() {
+		return maiores;
 	}
 	
 }
