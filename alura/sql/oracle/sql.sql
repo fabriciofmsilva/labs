0c44dcf9d374bb2b050d4fc3e7dfb70323d27f2d
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-09-19 23:36:20 -0300

    Alura, Introdução a Banco de Dados e SQL, Aula 1

---
+++ alura/sql/oracle/sql.sql
@@ -0,0 +1,16 @@
+create table compras (id number primary key, valor number, observacoes varchar2(30), data date, recebido char check (recebido in (0,1)));
+create sequence id_seq;
+insert into compras (id, valor, data, observacoes, recebido) values (id_seq.nextval, 100, '12-MAY-2007', 'Compras de maio', '1');
+select * from compras;
+select valor, data from compras;
+select valor * 3, data from compras;
+select valor * 3 as triplo, data from compras;
+insert into compras (id, valor, data, observacoes, recebido) values (id_seq.nextval, 150, '15-JUN-2007', 'Compras de junho', '0');
+select * from compras;
+select valor, data from compras where valor > 100;
+select valor, data from compras where valor < 120;
+select * from compras where valor < 120 and recebido = '1';
+select * from compras where valor < 120 or recebido = '1';
+select * from compras where observacoes = 'Compras';
+select * from compras where observacoes like 'Compras%';
+select * from compras where observacoes like '%de%';
