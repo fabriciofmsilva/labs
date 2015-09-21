c7c66ab4847a1aa8bcb1b939a57be7ee09e795f4
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-09-21 20:09:30 -0300

    Alura, Java 8, Aula 3

--- alura/java/java8/src/OrdenaStrings.java
+++ alura/java/java8/src/OrdenaStrings.java
@@ -23,10 +23,14 @@ public class OrdenaStrings {
 //			return 0;
 //		});
 		
-		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+//		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
+		palavras.sort(Comparator.comparing(s -> s.length()));
+//		palavras.sort(Comparator.comparing(String::length));
 
 		System.out.println(palavras);
 
+//		Consumer<String> impressor = System.out::println;
 //		Consumer<String> impressor = s -> System.out.println(s);
 //		palavras.forEach(impressor);
 
