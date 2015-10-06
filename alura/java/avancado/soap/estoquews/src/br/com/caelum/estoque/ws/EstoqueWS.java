42c7ed7bf332260ab8b651130e4b5d0b9d369aa0
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-10-05 21:03:44 -0300

    Alura, Web Services SOAP com JAX-WS, Aula 5

--- alura/java/avancado/soap/estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ alura/java/avancado/soap/estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -6,6 +6,8 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Use;
 
 import br.com.caelum.estoque.modelo.item.Filtro;
 import br.com.caelum.estoque.modelo.item.Filtros;
@@ -19,7 +21,8 @@ import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 import br.com.caelum.estoque.modelo.usuario.Usuario;
 
 @WebService
-public class EstoqueWS {
+@SOAPBinding(style=Style.DOCUMENT, parameterStyle=ParameterStyle.WRAPPED)
+public class EstoqueWS { // RPC
 
 	private ItemDao dao = new ItemDao();
 	
