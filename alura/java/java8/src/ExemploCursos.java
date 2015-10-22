6fb887d1828927286d93a494fcef85bf2bc659cf
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-10-22 21:53:00 -0200

    Alura, Java 8, Aula 5

--- alura/java/java8/src/ExemploCursos.java
+++ alura/java/java8/src/ExemploCursos.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 class Curso {
 	private String nome;
@@ -29,21 +33,23 @@ public class ExemploCursos {
 		cursos.add(new Curso("C", 55));
 
 		cursos.sort(Comparator.comparing(Curso::getAlunos));
-
-		cursos.stream()
-			.filter(c -> c.getAlunos() >= 100);
-			// 1
-//			.forEach(c -> System.out.println(c.getNome()));;
-			// 2
-//			.map(Curso::getAlunos)
-//			.forEach(System.out::println);
 		
-		int sum = cursos.stream()
+		OptionalDouble media = cursos.stream()
 			.filter(c -> c.getAlunos() >= 100)
 			.mapToInt(Curso::getAlunos)
-			.sum();
-		System.out.println(sum);
+			.average();
 
-//		cursos.forEach(c -> System.out.println(c.getNome()));
+//		cursos.stream()
+//			.filter(c -> c.getAlunos() >= 100)
+//			.findAny()
+//			.ifPresent(c -> System.out.println(c.getNome()));
+
+		cursos.parallelStream()
+			.filter(c -> c.getAlunos() >= 100)
+			.collect(Collectors.toMap(
+					c -> c.getNome(),
+					c -> c.getAlunos()))
+			.forEach((nome, alunos) -> System.out.println(nome + " tem " + alunos + " alunos"));
+		
 	}
 }
