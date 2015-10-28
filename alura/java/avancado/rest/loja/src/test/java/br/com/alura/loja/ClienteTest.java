477ed84cba0fd6d271746bce9cd57a3e3f2e6f78
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-10-28 20:20:35 -0200

    Alura, Web Services REST com JAX-RS e Jersey, Aula 5

--- alura/java/avancado/rest/loja/src/test/java/br/com/alura/loja/ClienteTest.java
+++ alura/java/avancado/rest/loja/src/test/java/br/com/alura/loja/ClienteTest.java
@@ -1,14 +1,13 @@
 package br.com.alura.loja;
 
-import java.net.URI;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +15,7 @@ import org.junit.Test;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.modelo.Projeto;
 import junit.framework.Assert;
 
@@ -49,10 +49,26 @@ public class ClienteTest {
         
 		Client client = ClientBuilder.newClient();
         WebTarget target = client.target("http://localhost:8080");
-        String conteudo = target.path("/projetos").request().get(String.class);
+        String conteudo = target.path("/projetos/1").request().get(String.class);
         Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
         Assert.assertEquals("Minha loja", projeto.getNome());
 
+	}
+	
+	@Test
+	public void testaOPost() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+		carrinho.setRua("Rua Vergueiro");
+		carrinho.setCidade("São Paulo");
+		String xml = carrinho.toXML();
+		
+		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+
+		Response response = target.path("/carrinhos").request().post(entity);
+		Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));
 	}
 	
 }
