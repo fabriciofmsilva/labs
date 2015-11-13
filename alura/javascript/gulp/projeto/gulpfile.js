6b3ce6a074058017557c76f39c7f48a1cf9b0d7d
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-11-12 23:55:11 -0200

    Alura, Gulp: automação de tarefas front-end, Aula 3

--- alura/javascript/gulp/projeto/gulpfile.js
+++ alura/javascript/gulp/projeto/gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var htmlReplace = require('gulp-html-replace');
+var uglify = require('gulp-uglify');
+var usemin = require('gulp-usemin');
+var cssmin = require('gulp-cssmin');
+
+gulp.task('default', ['copy'], function() {
+  gulp.start('build-img', 'usemin');
+});
 
 gulp.task('copy', ['clean'], function() {
   return gulp.src('src/**/*')
@@ -12,8 +21,17 @@ gulp.task('clean', function() {
     .pipe(clean());
 });
 
-gulp.task('build-img', ['copy'], function() {
+gulp.task('build-img', function() {
   gulp.src('dist/img/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function() {
+  gulp.src('dist/**/*.html')
+    .pipe(usemin({
+      'js': [uglify],
+      'css': [cssmin]
+    }))
+    .pipe(gulp.dest('dist'));
 });
