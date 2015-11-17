ee7fc6eb9e75205069fc3550681443be1fe186d1
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-11-16 22:25:44 -0200

    Alura, Eclipse: produtividade extrema, Aula 6

--- alura/java/eclipse/src/main/java/br/com/caelum/empresa/dao/GastoDAO.java
+++ alura/java/eclipse/src/main/java/br/com/caelum/empresa/dao/GastoDAO.java
@@ -1,9 +1,26 @@
 package br.com.caelum.empresa.dao;
 
-public class GastoDAO extends DAO<Gasto> {
+import java.util.List;
+import java.util.logging.Logger;
 
-	public GastoDAO() {
-		super(Gasto.class);
+import br.com.caelum.empresa.modelo.Gasto;
+
+public class GastoDAO {
+	private DAO<Gasto> dao = new DAO<Gasto>(Gasto.class);
+
+	public void adiciona(Gasto entity) {
+		dao.adiciona(entity);
 	}
 
+	public void remove(Gasto entity) {
+		dao.remove(entity);
+	}
+
+	public Gasto buscaPorId(Long id) {
+		return dao.buscaPorId(id);
+	}
+
+	public List<Gasto> buscaTodos() {
+		return dao.buscaTodos();
+	}
 }
