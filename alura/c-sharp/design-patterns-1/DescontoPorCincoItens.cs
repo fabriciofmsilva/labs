305c4baf9a7c3cd8e3d028e2ee7f0614d1714701
Fabrício Silva <fabriciofmsilva@gmail.com>
2015-11-18 21:50:10 -0200

    Alura, Design Patterns para bons programadores em C#, Aula 2

---
+++ alura/c-sharp/design-patterns-1/DescontoPorCincoItens.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns
+{
+  public class DescontoPorCincoItens : Desconto
+  {
+    public Desconto Proximo { get; set; }
+
+    public double Desconta(Orcamento orcamento)
+    {
+      if (orcamento.Itens.count > 5)
+      {
+        return orcamento.Valor * 0.1;
+      }
+
+      return Proximo.Desconta(orcamento);
+    }
+  }
+}
---
+++ alura/c-sharp/design-patterns-1/Item.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns
+{
+  public class Item
+  {
+    public String Nome { get; private set; }
+    public double Valor { get; private set; }
+
+    public Item(String nome, double valor)
+    {
+      this.Nome = nome;
+      this.valor = valor;
+    }
+  }
+}
