03611751c551f703a5d604cfccce2c42fc9020a5
Fabrício Silva <fabriciofmsilva@gmail.com>
2016-01-24 23:22:04 -0200

    Alura, NodeJS, aula 3

--- alura/javascript/node/casadocodigo/app/routes/produtos.js
+++ alura/javascript/node/casadocodigo/app/routes/produtos.js
@@ -1,15 +1,33 @@
 module.exports = function(app) {
-  app.get('/produtos', function(req, res) {
+  var listaProdutos = function(req, res) {
 
     var connection = app.infra.connectionFactory();
-    var produtosBanco = new app.infra.ProdutosDAO(connection);
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-    produtosBanco.lista(function(err, results) {
+    produtosDAO.lista(function(err, results) {
       res.render('produtos/lista', {
         lista: results
       });
     });
 
     connection.end();
+  };
+
+  app.get('/produtos', listaProdutos);
+
+  app.get('/produtos/form', function(req, res) {
+    res.render('produtos/form');
+  });
+
+  app.post('/produtos', function(req, res) {
+
+    var produto = req.body;
+    console.log(produto);
+
+    var connection = app.infra.connectionFactory();
+    var produtosDAO = new app.infra.ProdutosDAO(connection);
+    produtosDAO.salva(produto, function(erros, resultados) {
+      res.redirect('/produtos');
+    });
   });
 }
