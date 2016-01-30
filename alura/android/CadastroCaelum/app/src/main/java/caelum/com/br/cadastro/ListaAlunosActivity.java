8af21d5dd00eecc0ad868f20a68dbe2d1e6f92c3
Fabrício Silva <fabriciofmsilva@gmail.com>
2016-01-30 15:41:12 -0200

    Alura, Android I, aula 3

---
+++ alura/android/CadastroCaelum/app/src/main/java/caelum/com/br/cadastro/ListaAlunosActivity.java
@@ -0,0 +1,46 @@
+package caelum.com.br.cadastro;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+/**
+ * Created by fabriciosilva on 1/30/16.
+ */
+public class ListaAlunosActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listagem_alunos);
+
+        ListView lista = (ListView) findViewById(R.id.lista);
+
+        final String[] alunos = {"Suelen", "Guilherme", "Maurício"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
+
+        lista.setAdapter(adapter);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+                Toast.makeText(ListaAlunosActivity.this, "A posição é " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View view, int position, long id) {
+                Toast.makeText(ListaAlunosActivity.this, "Aluno clicado é " + adapter.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+
+                return true;
+            }
+        });
+    }
+
+}
