bfeb78d2bdbecbe7f1dca2c582872bc52cd119a5
Fabrício Silva <fabriciofmsilva@gmail.com>
2016-02-17 21:19:11 -0200

    Alura, Java e JPA, aula 2

---
+++ alura/java/web/jpa-hibernate/financas/src/br/com/caelum/financas/teste/TesteJPA.java
@@ -0,0 +1,32 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.caelum.financas.modelo.Conta;
+
+public class TesteJPA {
+
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta();
+		conta.setTitular("Joao Ferreira");
+		conta.setBanco("HSBC");
+		conta.setNumero("12345");
+		conta.setAgencia("321");
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("financas");
+		EntityManager manager = entityManagerFactory.createEntityManager();
+
+		manager.getTransaction().begin();
+		
+		manager.persist(conta);
+		
+		manager.getTransaction().commit();
+		
+		manager.close();
+
+	}
+
+}
