455398022771151551be8374ce40dc11f73067b4
Fabrício Silva <fabriciofmsilva@gmail.com>
2016-02-18 00:16:53 -0200

    Alura, Java e JPA, aula 4

--- alura/java/web/jpa-hibernate/financas/src/br/com/caelum/financas/teste/TesteJPA.java
+++ alura/java/web/jpa-hibernate/financas/src/br/com/caelum/financas/teste/TesteJPA.java
@@ -1,8 +1,13 @@
 package br.com.caelum.financas.teste;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
+
 import javax.persistence.EntityManager;
 
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
 import br.com.caelum.financas.util.JPAUtil;
 
 public class TesteJPA {
@@ -10,20 +15,25 @@ public class TesteJPA {
 	public static void main(String[] args) {
 		
 		Conta conta = new Conta();
-		conta.setId(7);
-		conta.setTitular("Mario Santos");
+		conta.setTitular("Ana Maria");
 		conta.setBanco("ITAU");
 		conta.setNumero("54321");
-		conta.setAgencia("3344");
+		conta.setAgencia("111");
+		
+		Movimentacao movimentacao = new Movimentacao();
+		movimentacao.setData(Calendar.getInstance());
+		movimentacao.setDescricao("Conta de luz");
+		movimentacao.setTipoMovimentacao(TipoMovimentacao.SAIDA);
+		movimentacao.setValor(new BigDecimal("123.9"));
+		
+		movimentacao.setConta(conta);
 		
 		EntityManager em = new JPAUtil().getEntityManager();
 		em.getTransaction().begin();
-		
-		Conta contaARemover = em.find(Conta.class, 7);
-		
-		em.remove(contaARemover);
-		
-		System.out.println(conta.getId());
+
+		em.persist(conta);
+
+		em.persist(movimentacao);
 		
 		em.getTransaction().commit();
 		em.close();
---
+++ alura/java/web/jpa-hibernate/financas/src/br/com/caelum/financas/teste/TesteJPARelacionamento.java
@@ -0,0 +1,31 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteJPARelacionamento {
+
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta();
+		conta.setId(7);
+		conta.setTitular("Mario Santos");
+		conta.setBanco("ITAU");
+		conta.setNumero("54321");
+		conta.setAgencia("3344");
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+		em.getTransaction().begin();
+		
+		Conta contaARemover = em.find(Conta.class, 7);
+		
+		em.remove(contaARemover);
+		
+		System.out.println(conta.getId());
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+}
