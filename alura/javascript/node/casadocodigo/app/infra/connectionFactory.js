822be4885d6705203e442c3ff2384212e7684cdb
Fabrício Silva <fabriciofmsilva@gmail.com>
2016-04-02 17:44:17 -0300

    Alura, NodeJS, aula 7

--- alura/javascript/node/casadocodigo/app/infra/connectionFactory.js
+++ alura/javascript/node/casadocodigo/app/infra/connectionFactory.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 function createDBConnection() {
-  if (!process.env.NODE_ENV) {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     return mysql.createConnection({
       host: 'localhost',
       user: 'root',
