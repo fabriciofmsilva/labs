f86ec37563e67aeb8601c636680f96f2e9d2bfe0
Fabrício Silva <fabriciofmsilva@gmail.com>
2016-05-02 21:33:34 -0300

    Alura, JavaScript: avançando com uma webapp na prática, Aula 4

--- alura/javascript/webapp/javascript/block/card.js
+++ alura/javascript/webapp/javascript/block/card.js
@@ -14,21 +14,30 @@
 // }
 
 var $cards = document.querySelectorAll(".card");
-var $cardColors = document.querySelectorAll(".card-colors");
+var $cardColors = document.querySelectorAll(".card-options");
 
 for (var index = 0; index < $cards.length; index++) {
   $cards[index].addEventListener("click", function(event) {
     var $this = event.target;
+    var $card = this;
+    var $cardContent = $card.querySelector(".card-content");
 
-    if ($this.classList.contains("card-colors")) {
-      var $card = this;
-
+    if ($this.dataset.color) {
       $card.dataset.color = $this.dataset.color;
 
       for (var i = 0; i < $cardColors.length; i++) {
         $cardColors[i].classList.remove("isActive");
       }
       $this.classList.add("isActive");
+    }
+
+    if ($this.classList.contains("card_delete")) {
+      $card.remove();
+    }
+
+    if ($this.classList.contains("card_edit")) {
+      $cardContent.setAttribute("contenteditable", "true");
+      $cardContent.focus();
     }
   });
 }
