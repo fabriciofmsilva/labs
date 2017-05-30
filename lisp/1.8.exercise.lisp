cf9462b7943ebc6cabb0dd6b0b0ee7ca776bbfa3
Fabrício Silva <fabriciofmsilva@gmail.com>
2017-05-29 22:52:43 -0300

    added exercises

---
+++ lisp/1.8.exercise.lisp
@@ -0,0 +1,26 @@
+; 1.8 exercise
+
+(define (cube-root-iter guess old-guess x)
+    (if (good-enough? guess old-guess)
+        guess
+        (cube-root-iter (improve guess x)
+                   guess
+                   x)))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess))
+        (* 2 guess))
+   3))
+
+(define (square x) (* x x))
+
+(define (average x y)
+    (/ (+ x y) 2))
+
+(define (good-enough? guess old-guess)
+  (< (abs (- guess old-guess)) 0.001))
+
+(define (cube-root x)
+  (cube-root-iter 1.0 0 x))
+
+(cube-root 25)
